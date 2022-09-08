a43bb97912a976d00e84d726dd47d396bfc5ee89
BhargavKrixi <bhargav.simejiya@krixi.com>
2022-09-08 12:38:34 +0530

    test trigger build 2

--- App.js
+++ App.js
@@ -73,7 +73,7 @@ const App: () => Node = () => {
           <Section title="Step One">
             React Native <Text style={styles.highlight}>Bitrise</Text> Seup.
           </Section>
-          <Section title="See Your Changess">
+          <Section title="See Your Changessss">
             <ReloadInstructions />
           </Section>
           <Section title="Debug">
